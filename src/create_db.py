b4c714d44e29284aac11e208cb38e51c4138cf86
owen0621 <owen10270936@gmail.com>
2023-05-30 23:51:38 +0800

    feat: construct db

--- src/create_db.py
+++ src/create_db.py
@@ -1,32 +1,31 @@
 import mysql.connector
 import os
+import cv2
+import random
+from tqdm import tqdm
+from multiprocessing import set_start_method, Pool
+
 from Min_hash import minhash
 from BiCE_descriptor import compute_BiCE_descriptor
 from Edge_extraction import long_edge_extraction, canny
-import cv2
-import random
 
-from multiprocessing import set_start_method, Pool
-
-
-# 建立数据库连接
+# create db connection
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
     password="password",
     database="SHADOWDRAW",
 )
-# 创建游标对象
+# create cursor and read seeds
 cursor = connection.cursor()
 query = "SELECT `SEED1`, `SEED2`, `SEED3` FROM `HASH_FUNC`"
 cursor.execute(query)
 seeds = cursor.fetchall()
 cursor.close()
-connection.close()
-
-# pool = Pool()
+# connection.close()
 
 
+# convert a patch to sketches
 def patch2sketches(patch):
     descriptor = compute_BiCE_descriptor(patch)
     sketches = []
@@ -49,15 +48,20 @@ def patch2sketches(patch):
     return sketches
 
 
+# convert a image to sketches
 def img2sketches(img, img_size=300, patch_size=60, over_lap=0.5):
     edge = canny(img, (img_size, img_size))
     # edge = long_edge_extraction(img, (img_size, img_size))
 
     patchs = [
-        edge[y : y + patch_size, x : x + patch_size]
-        for y in range(0, img_size - patch_size + 1, int(patch_size * over_lap))
+        edge[x : x + patch_size, y : y + patch_size]
         for x in range(0, img_size - patch_size + 1, int(patch_size * over_lap))
+        for y in range(0, img_size - patch_size + 1, int(patch_size * over_lap))
     ]
+    # for patch in patchs:
+    #     cv2.imshow("My Image", patch)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
     result = pool.map(patch2sketches, patchs)
     return result
 
@@ -65,42 +69,54 @@ def img2sketches(img, img_size=300, patch_size=60, over_lap=0.5):
 if __name__ == "__main__":
     set_start_method("fork", True)
     pool = Pool()
+    cursor = connection.cursor()
+
+    img_size = 300
+    patch_size = 60
+    over_lap = 0.5
+    coor = [
+        (x, y)
+        for y in range(0, img_size - patch_size + 1, int(patch_size * over_lap))
+        for x in range(0, img_size - patch_size + 1, int(patch_size * over_lap))
+    ]
+
     categories = os.listdir("../images")
-    for cate in categories:
-        if cate != "cat":
-            break
+    for cate in tqdm(categories):
         curdir = "../images/" + cate + "/"
-        img_names = list(map(lambda x: curdir + x, os.listdir(curdir)))
-        for img_name in img_names:
-            img = cv2.imread(img_name)
-            sketchs = img2sketches(img)
-            break
-
-
-# # 插入 IMG 数据
-# img_data = [("image1.jpg", "cat1"), ("image2.jpg", "cat2"), ("image3.jpg", "cat1")]
-# img_insert_query = "INSERT INTO IMG (FNAME, CAT) VALUES (%s, %s)"
-# cursor.executemany(img_insert_query, img_data)
-
-# # 插入 SKETCH 数据
-# sketch_data = [
-#     ("image1.jpg", 1, 10, 20, 30),
-#     ("image1.jpg", 2, 15, 25, 35),
-#     ("image2.jpg", 1, 12, 22, 32),
-# ]
-# sketch_insert_query = "INSERT INTO SKETCH (FNAME, `INDEX`, VALUE1, VALUE2, VALUE3) VALUES (%s, %s, %s, %s, %s)"
-# cursor.executemany(sketch_insert_query, sketch_data)
-
-# # 插入 HASH_FUNC 数据
-# hash_func_data = [(1, 123, 456, 789), (2, 987, 654, 321)]
-# hash_func_insert_query = (
-#     "INSERT INTO HASH_FUNC (`INDEX`, SEED1, SEED2, SEED3) VALUES (%s, %s, %s, %s)"
-# )
-# cursor.executemany(hash_func_insert_query, hash_func_data)
-
-# # 提交事务
-# connection.commit()
-
-# # 关闭游标和数据库连接
-# cursor.close()
-# connection.close()
+        img_names = os.listdir(curdir)
+        for img_name in tqdm(img_names):
+            img_path = curdir + img_name
+            img = cv2.imread(img_path)
+            sketchs = img2sketches(img, img_size, patch_size, over_lap)
+            # 插入 IMG 数据
+            img_data = [(img_name, cate)]
+            img_insert_query = "INSERT INTO IMG (FNAME, CATE) VALUES (%s, %s)"
+            cursor.executemany(img_insert_query, img_data)
+            # 插入 PATCH 数据
+            patch_data = [
+                (img_name, i, coor[i][0], coor[i][1]) for i in range(len(sketchs))
+            ]
+            patch_insert_query = (
+                "INSERT INTO PATCH (FNAME, `ID`, DX, DY) VALUES (%s, %s, %s, %s)"
+            )
+            cursor.executemany(patch_insert_query, patch_data)
+            # 插入 SKETCH 数据
+            sketch_data = []
+            for i in range(len(sketchs)):
+                for j in range(len(sketchs[0])):
+                    sketch_data.append(
+                        (
+                            img_name,
+                            i,
+                            j,
+                            sketchs[i][j][0],
+                            sketchs[i][j][1],
+                            sketchs[i][j][2],
+                        )
+                    )
+            sketch_insert_query = "INSERT INTO SKETCH (FNAME, `ID`, `INDEX`, `VALUE1`, `VALUE2`, `VALUE3`) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.executemany(sketch_insert_query, sketch_data)
+            # 提交事务
+            connection.commit()
+    cursor.close()
+    connection.close()
